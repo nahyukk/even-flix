c76798734f3b40987e60b49f2071a83f1d57a8db
seongmin <min@seongminui-MacBookPro.local>
2025-01-13 15:32:04 +0900

    예시 텍스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">이븐하게 시작</h1>
+		</>
+	);
 }
 
 export default App;
