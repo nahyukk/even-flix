44ab2f79f6c7f2368b8a7eb1644a28288fc6d8a0
SangHwi <brilyent72@gmail.com>
2025-01-14 16:16:26 +0900

    NavBar.tsx Dom 트리

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,22 @@
+import React from 'react';
+
 const NavBar = () => {
     return (
-      <header>
         <nav>
-          <ul>
-            <li><a href="/">홈</a></li>
-            <li><a href="/about">시리즈</a></li>
-            <li><a href="/contact">영화</a></li>
-            <li><a href="/contact">NEW! 요즘 대세 콘텐츠</a></li>
-            <li><a href="/contact">내가 찜한 리스트</a></li>
-            <li><a href="/contact">언어별로 찾아보기</a></li>
-          </ul>
+            <div>
+                <a></a>
+                <ul>
+                    <li><a href="/browse">홈</a></li>
+                    <li><a href="/browse/genre/83">시리즈</a></li>
+                    <li><a href="/browse/genre/34399">영화</a></li>
+                    <li><a href="/latest">NEW! 요즘 대세 콘텐츠</a></li>
+                    <li><a href="/browse/my-list">내가 찜한 리스트</a></li>
+                    <li><a href="/browse/original-audio">언어별로 찾아보기</a></li>
+                </ul>
+                <div></div>
+            </div>
         </nav>
-      </header>
     );
-  };
-  
-  export default NavBar;
+};
+
+export default NavBar;
