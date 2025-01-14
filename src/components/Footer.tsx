862887ef3f17b1b096892489d7cbdd90ae4d8155
SangHwi <brilyent72@gmail.com>
2025-01-14 15:43:38 +0900

    tsx 기본 가이드 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,9 @@
-1
+const Footer = () => {
+    return (
+      <footer>
+        <p>&copy; 2025 My Project. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,1 +1,18 @@
-1
+const NavBar = () => {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">홈</a></li>
+            <li><a href="/about">시리즈</a></li>
+            <li><a href="/contact">영화</a></li>
+            <li><a href="/contact">NEW! 요즘 대세 콘텐츠</a></li>
+            <li><a href="/contact">내가 찜한 리스트</a></li>
+            <li><a href="/contact">언어별로 찾아보기</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default NavBar;
