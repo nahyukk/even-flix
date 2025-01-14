c8c71f269b32ab0ec130c438934a6eb835b453a1
seongmin <min@seongminui-MacBookPro.local>
2025-01-14 12:52:14 +0900

    이미지 하단 그림자 추가

--- src/components/DetailModal/page.tsx
+++ src/components/DetailModal/page.tsx
@@ -38,6 +38,15 @@ const DetailModal = ({
 							/>
 						</svg>
 					</span>
+					<div className="modal__poster-container relative">
+						<div className="modal__poster-blur absolute bottom-0 inset-x-0 h-1/2 bg-gradient-to-t from-neutral-900"></div>
+						<img
+							className="modal__poster-img w-full h-auto"
+							src="https://image.tmdb.org/t/p/original/2JB2nJQOBZ7Q9vAtzbha2YsqJbH.jpg"
+							alt="modal__poster-img"
+						></img>
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
