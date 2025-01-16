30e3ca2035139e0bf7982a14a55221348332820e
seongmin <min@seongminui-MacBookPro.local>
2025-01-16 15:20:53 +0900

    함께 시청된 콘텐츠 섹션 추가

--- src/components/DetailModal/components/ModalRecommends.tsx
+++ src/components/DetailModal/components/ModalRecommends.tsx
@@ -9,7 +9,7 @@ interface ModalSimilarsProps {
 const ModalSimilars: FC<ModalSimilarsProps> = ({ recommends }) => {
 	return (
 		<div className="px-12 py-8">
-			<p className="text-xl">함께 시청된 콘텐츠</p>
+			<p className="text-xl font-semibold">함께 시청된 콘텐츠</p>
 			<div className="grid grid-cols-2 gap-4 pt-4 sm:grid-cols-3">
 				{recommends.map((recommend) => (
 					<ModalRecommend recommend={recommend} />
