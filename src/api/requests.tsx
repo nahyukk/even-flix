d1a0cf727fdcc1b87b0e7a5687260bc35823298c
nahyun <knahyuk@gmail.com>
2025-01-16 16:14:56 +0900

    홈화면에서 사용할 데이터 url requests 추가(지금 상영, 인기 영화)

--- src/api/requests.tsx
+++ src/api/requests.tsx
@@ -8,6 +8,12 @@ const requests = {
     fetchHorrorMovies: "/discover/movie?with_genres=27",
     fetchRomanceMovies: "/discover/movie?with_genres=10749",
     fetchDocumentaries: "/discover/movie?with_genres=99",
+
+
+		// 홈화면에서 사용하는 부분 (위와 중복이 있을 수도 있지만 헷갈리지 않도록 이름을 다르게 했습니다.)
+		fetchNowPlayingMovies: "/movie/now_playing",
+		fetchPopularMoviesData: "/movie/popular"
+
 }
 
 export default requests;
