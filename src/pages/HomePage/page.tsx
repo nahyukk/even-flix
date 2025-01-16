3479ced1c1b2ff98d35e4f4becd5a785c0afca67
nahyun <knahyuk@gmail.com>
2025-01-17 00:34:54 +0900

    홈페이지 - ForYou, 오늘 TOP 10 시리즈KR, 리얼리티 시리즈, 감명 시리즈, 영어 컨텐츠, 미국영화, 한국시리즈, 전세계 TOP 10 영화, 지금 상영 중인 영화, SF & 판타지 섹션 추가 및 API 데이터 연결

--- src/pages/HomePage/page.tsx
+++ src/pages/HomePage/page.tsx
@@ -10,19 +10,37 @@ interface MovieOrSeries {
   id: number;
   backdrop_path: string;
   poster_path: string;
+  origin_country?: string[];
 }
 
 const HomePage: React.FC = () => {
-  const [nowPlayingMovies, setNowPlayingMovies] = useState<MovieOrSeries[]>([]);
+  const [forYou, setForYou] = useState<MovieOrSeries[]>([]);
+  const [todayTop10SeriesKR, setTodayTop10SeriesKR] = useState<MovieOrSeries[]>(
+    []
+  );
+  const [realitySeries, setRealitySeries] = useState<MovieOrSeries[]>([]);
+  const [topRatedSeries, setTopRatedSeries] = useState<MovieOrSeries[]>([]);
+  const [englishContents, setEnglishContents] = useState<MovieOrSeries[]>([]);
+  const [uSMovies, setUSMovies] = useState<MovieOrSeries[]>([]);
+  const [koreanSeries, setKoreanSeries] = useState<MovieOrSeries[]>([]);
   const [popularMovies, setPopularMovies] = useState<MovieOrSeries[]>([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState<MovieOrSeries[]>([]);
+  const [sFAndFantasySeries, setSFAndFantasySeries] = useState<MovieOrSeries[]>(
+    []
+  );
 
   // fetchDataHome 컴포넌트는 fetchData로 따로 컴포넌트화 시켜서 재사용할 수 있지만
   // 각 page에서 개인 api 연결 공부를 위해 page에 넣음
   const fetchDataHome = async (url: string) => {
     try {
-      console.log("요청 URL:", url);
       const response = await instance.get(url);
-      return response.data.results;
+      const results = response.data.results;
+
+      const filteredResults = results.filter(
+        (item: any) => item.backdrop_path !== null
+      );
+
+      return filteredResults;
     } catch (error) {
       console.log("API 요청 에러", error);
       return [];
@@ -32,23 +50,144 @@ const HomePage: React.FC = () => {
   useEffect(() => {
     const fetchHome = async () => {
       try {
-				// 1페이지에 20개 라서 40개 불러올 경우 여러 페이지를 불러와야함.
-				const nowPlayingMoviePage1 = await fetchDataHome(
+        // 회원님을 위해 엄선한 오늘의 콘텐츠
+        const forYouPage1 = await fetchDataHome(
+          `${requests.fetchForYou}?page=1`
+        );
+        const forYouPage2 = await fetchDataHome(
+          `${requests.fetchForYou}?page=2`
+        );
+        const forYouPage3 = await fetchDataHome(
+          `${requests.fetchForYou}?page=3`
+        );
+
+        const forYouData = [...forYouPage1, ...forYouPage2, ...forYouPage3];
+
+        // 오늘 대한민국의 TOP 10 시리즈
+        const todayTop10SeriesData = await fetchDataHome(
+          requests.fetchTodayTop10Series
+        );
+
+        const todayTop10SeriesDataKR = todayTop10SeriesData.filter(
+          (item: MovieOrSeries) => item.origin_country?.includes("KR")
+        );
+
+        let todayTop10SeriesFinalKR: MovieOrSeries[] = [];
+        if (todayTop10SeriesDataKR.length >= 10) {
+          todayTop10SeriesFinalKR = todayTop10SeriesDataKR;
+        } else {
+          const needed = 10 - todayTop10SeriesDataKR.length;
+          const nonKrSeries = todayTop10SeriesData.filter(
+            (item: MovieOrSeries) => !item.origin_country?.includes("KR")
+          );
+          const fillOthers = nonKrSeries.slice(0, needed);
+          todayTop10SeriesFinalKR = [...todayTop10SeriesDataKR, ...fillOthers];
+        }
+
+        // 재미를 주는 리얼리티 시리즈
+        const realitySeriesPage1 = await fetchDataHome(
+          `${requests.fetchRealitySeries}?language=ko-KR&page=1`
+        );
+        const realitySeriesPage2 = await fetchDataHome(
+          `${requests.fetchRealitySeries}?language=ko-KR&page=2`
+        );
+        const realitySeriesPage3 = await fetchDataHome(
+          `${requests.fetchRealitySeries}?language=ko-KR&page=3`
+        );
+
+        const realitySeriesData = [
+          ...realitySeriesPage1,
+          ...realitySeriesPage2,
+          ...realitySeriesPage3,
+        ];
+
+        // 평단의 찬사! 감명을 주는 시리즈
+        const topRatedSeriesData = await fetchDataHome(
+          requests.fetchTopRatedSeries
+        );
+
+        // 즐거운 영어 세상 속으로!
+        const englishContentsData = await fetchDataHome(
+          requests.fetchEnglishContents
+        );
+
+        // 미국 영화
+        const uSMoviesPage1 = await fetchDataHome(
+          `${requests.fetchUSMovies}?page=1`
+        );
+        const uSMoviesPage2 = await fetchDataHome(
+          `${requests.fetchUSMovies}?page=2`
+        );
+        const uSMoviesPage3 = await fetchDataHome(
+          `${requests.fetchUSMovies}?page=3`
+        );
+
+        const uSMoviesData = [
+          ...uSMoviesPage1,
+          ...uSMoviesPage2,
+          ...uSMoviesPage3,
+        ];
+
+        // 한국 시리즈
+        const koreanSeriesPage1 = await fetchDataHome(
+          `${requests.fetchKoreanSeries}?page=1`
+        );
+        const koreanSeriesPage2 = await fetchDataHome(
+          `${requests.fetchKoreanSeries}?page=2`
+        );
+        const koreanSeriesPage3 = await fetchDataHome(
+          `${requests.fetchKoreanSeries}?page=3`
+        );
+
+        const koreanSeriesData = [
+          ...koreanSeriesPage1,
+          ...koreanSeriesPage2,
+          ...koreanSeriesPage3,
+        ];
+
+        // 오늘 전세계의 TOP 10 영화
+        const popularMoviesData = await fetchDataHome(
+          requests.fetchPopularMovies
+        );
+
+        // 지금 상영 중인 영화
+        // 1페이지에 20개 라서 40개 불러올 경우 여러 페이지를 불러와야함.
+        const nowPlayingMoviePage1 = await fetchDataHome(
           `${requests.fetchNowPlayingMovies}?language=ko-KR&page=1`
         );
         const nowPlayingMoviePage2 = await fetchDataHome(
           `${requests.fetchNowPlayingMovies}?language=ko-KR&page=2`
         );
 
-        const nowPlayingMovieData = [...nowPlayingMoviePage1, ...nowPlayingMoviePage2];
+        const nowPlayingMovieData = [
+          ...nowPlayingMoviePage1,
+          ...nowPlayingMoviePage2,
+        ];
 
-        const popularMoviesData = await fetchDataHome(
-          requests.fetchPopularMoviesData
+        // SF &  판타지 시리즈
+        const sFAndFantasySeriesPage1 = await fetchDataHome(
+          `${requests.fetchSFAndFantasySeries}?page=1`
+        );
+        const sFAndFantasySeriesPage2 = await fetchDataHome(
+          `${requests.fetchSFAndFantasySeries}?page=2`
         );
 
-        setNowPlayingMovies(nowPlayingMovieData);
-				// 1페이지에 20개인데 top10 이라 10개만 불러올 경우
+        const sFAndFantasySeriesData = [
+          ...sFAndFantasySeriesPage1,
+          ...sFAndFantasySeriesPage2,
+        ];
+
+        setForYou(forYouData);
+        setTodayTop10SeriesKR(todayTop10SeriesFinalKR.slice(0, 10));
+        setRealitySeries(realitySeriesData);
+        setTopRatedSeries(topRatedSeriesData);
+        setEnglishContents(englishContentsData);
+        setUSMovies(uSMoviesData);
+        setKoreanSeries(koreanSeriesData);
+        // 1페이지에 20개인데 top10 이라 10개만 불러올 경우
         setPopularMovies(popularMoviesData.slice(0, 10));
+        setNowPlayingMovies(nowPlayingMovieData);
+        setSFAndFantasySeries(sFAndFantasySeriesData.slice(0, 36));
       } catch (error) {
         console.log("데이터 못 가져왔지롱~", error);
       }
@@ -61,19 +200,76 @@ const HomePage: React.FC = () => {
       <div className="banner relative z-0 h-[630px]">
         <Banner />
       </div>
-      <div className="mainbody relative z-20 pb-10">
+      <div className="mainbody relative z-5 pb-10">
         <CardList
-          title="지금 상영중인 영화"
-          moviesAndSeries={nowPlayingMovies.map((movie) => ({
-            id: movie.id,
-            backdropUrl: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+          title="회원님을 위해 엄선한 오늘의 콘텐츠"
+          moviesAndSeries={forYou.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+        <PosterList
+          title="오늘 대한민국의 TOP 10 시리즈"
+          moviesAndSeries={todayTop10SeriesKR.map((item) => ({
+            id: item.id,
+            posterUrl: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+          }))}
+        />
+        <CardList
+          title="재미를 주는 리얼리티 시리즈"
+          moviesAndSeries={realitySeries.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+        <CardList
+          title="평단의 찬사! 감명을 주는 시리즈"
+          moviesAndSeries={topRatedSeries.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+        <CardList
+          title="즐거운 영어 세상 속으로!"
+          moviesAndSeries={englishContents.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+        <CardList
+          title="미국 영화"
+          moviesAndSeries={uSMovies.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+
+        <CardList
+          title="한국 시리즈"
+          moviesAndSeries={koreanSeries.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
           }))}
         />
         <PosterList
           title="오늘 전세계의 TOP 10 영화"
-          moviesAndSeries={popularMovies.map((movie) => ({
-            id: movie.id,
-            posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          moviesAndSeries={popularMovies.map((item) => ({
+            id: item.id,
+            posterUrl: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+          }))}
+        />
+        <CardList
+          title="지금 상영중인 영화"
+          moviesAndSeries={nowPlayingMovies.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
+          }))}
+        />
+        <CardList
+          title="SF & 판타지 시리즈"
+          moviesAndSeries={sFAndFantasySeries.map((item) => ({
+            id: item.id,
+            backdropUrl: `https://image.tmdb.org/t/p/w500${item.backdrop_path}`,
           }))}
         />
       </div>
