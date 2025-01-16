d72491a90bd1f503f71642ea453b822121b6f723
nahyun <knahyuk@gmail.com>
2025-01-16 16:17:07 +0900

    App에 레이아웃(네비, 푸터) 추가 및 홈화면 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "./App.css";
-import Banner from "./components/Banner";
+
+import { Outlet, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage/page";
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
-	return (
-		<div>
-			<Banner/>
-		</div>
-
-);
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="browse" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
