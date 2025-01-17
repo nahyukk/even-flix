3e25cab89ad68e75a8e25e4adc2e43042589adae
seongmin <min@seongminui-MacBookPro.local>
2025-01-17 13:00:11 +0900

    keyword 모델 변경

--- src/models/Keyword.ts
+++ src/models/Keyword.ts
@@ -1,17 +1,21 @@
-export interface Keywords {
-	id: number;
-	keywords: Keyword[];
-}
-
 export interface Keyword {
 	id: number;
 	name: string;
 }
 
-export const mapKeywords = (json: any): Keywords => ({
-	id: json.id,
-	keywords: json.keywords.map((keyword: any) => ({
-		id: keyword.id,
-		name: keyword.name,
-	})),
-});
+export const mapKeywords = (json: any): Keyword[] => {
+	if (json.keywords) {
+		return json.keywords.map((keyword: any) => ({
+			id: keyword.id,
+			name: keyword.name,
+		}));
+	}
+
+	if (json.results) {
+		return json.results.map((keyword: any) => ({
+			id: keyword.id,
+			name: keyword.name,
+		}));
+	}
+	return []; // keywords와 results 모두 없으면 빈 배열 반환
+};
