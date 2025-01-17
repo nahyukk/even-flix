8e9f858b7f3e2a7cea40d401d9066cbcd3750026
seongmin <min@seongminui-MacBookPro.local>
2025-01-17 18:32:47 +0900

    검색 UI 구현

--- src/pages/SearchPage/page.tsx
+++ src/pages/SearchPage/page.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import useDebounce from "../../hooks/useDebounce";
 import axios from "../../api/axios";
 import { mapSearch, Media } from "../../models/Media";
+import Card from "../../components/Card";
 
 const SearchPage = () => {
 	const useQuery = () => {
@@ -24,18 +25,28 @@ const SearchPage = () => {
 			const request = await axios.get(
 				`/search/multi?include_adult=false&query=${query}`
 			);
-			console.log(request);
 			const mappedResult = mapSearch(request.data);
-			console.log(mappedResult);
+			setSearchResults(mappedResult);
 		} catch (error) {
 			console.log("Error: ", error);
 		}
 	};
 
+	//
 	return (
 		<div>
 			{searchResults.length > 0 ? (
-				<div>YES</div>
+				<div className="px-12 py-8">
+					<div className="grid gap-x-2 gap-y-8 grid-cols-2 sm:grid-cols-3 md:grid-col-3 xl:grid-cols-5">
+						{searchResults.map((result) => (
+							<Card
+								key={result.id}
+								id={result.id}
+								backdropPath={`https://image.tmdb.org/t/p/original/${result.backdropPath}`}
+							/>
+						))}
+					</div>
+				</div>
 			) : (
 				<div className="flex flex-col pt-12 pb-96 justify-center items-center text-xs">
 					<div>
