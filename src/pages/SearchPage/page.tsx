8ff83b01444e53f86de766a08b751b73215807df
seongmin <min@seongminui-MacBookPro.local>
2025-01-17 18:12:42 +0900

    검색 시 api 호출

--- src/pages/SearchPage/page.tsx
+++ src/pages/SearchPage/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import useDebounce from "../../hooks/useDebounce";
+import axios from "../../api/axios";
+import { mapSearch, Media } from "../../models/Media";
 
 const SearchPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const useQuery = () => {
+		return new URLSearchParams(useLocation().search);
+	};
+	const query = useQuery().get("q");
+	const debouncedQuery = useDebounce(query, 500);
 
-export default SearchPage
+	const [searchResults, setSearchResults] = useState<Media[]>([]);
+
+	useEffect(() => {
+		if (debouncedQuery) {
+			fetchSearch(debouncedQuery);
+		}
+	}, [debouncedQuery]);
+
+	const fetchSearch = async (query: string) => {
+		try {
+			const request = await axios.get(
+				`/search/multi?include_adult=false&query=${query}`
+			);
+			console.log(request);
+			const mappedResult = mapSearch(request.data);
+			console.log(mappedResult);
+		} catch (error) {
+			console.log("Error: ", error);
+		}
+	};
+
+	return (
+	);
+};
+
+export default SearchPage;
