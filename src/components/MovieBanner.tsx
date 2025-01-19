b854cc13b6e5c81e8728c61d8d7b44088ded8482
LJIEUN <eun773c@daum.net>
2025-01-20 01:41:43 +0900

    기본 배너 이미지 -> 영화 배너 이미지로 변경

---
+++ src/components/MovieBanner.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import requests from "../api/requests";
+
+interface Movie {
+    backdrop_path: string;
+    title?: string;
+    original_title?: string;
+    overview?: string;
+    videos?: {
+        results: { key: string }[];
+    };
+}
+
+interface Genre {
+    id: number;
+    name: string;
+}
+
+export default function MovieBanner() {
+    const [movie, setmovie] = useState<Movie | null>(null);
+    const [isClicked, setisClicked] = useState(false);
+    const [genres, setgenres] = useState<Genre[]>([]);
+    const [isDropdownOpen, setisDropdownOpen] = useState(false);
+
+    useEffect(() => {
+        fetchData();
+        
+    }, []);
+
+    const fetchData = async () => {
+        const request = await axios.get(requests.fetchMovies);
+        const movieId = request.data.results[Math.floor(Math.random() * request.data.results.length)].id;
+
+        const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+            params: {
+                append_to_response: "videos",
+            },
+        });
+        setmovie(movieDetail);
+    };
+
+    const truncate = (str: string | undefined, n: number) => {
+        return str && str.length > n ? str.substring(0, n - 1) + "..." : str;
+    };
+}
