cea04d1b1e47717a58d8fe2e674c74cb0a01c371
SangHwi <brilyent72@gmail.com>
2025-01-20 00:56:30 +0900

    서브 헤더 정렬 기준 드롭다운 목록 구현 및 연동

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -5,17 +5,22 @@ const SubHeader = () => {
     const [selectedViewMode, setSelectedViewMode] = useState('원어');
     const [langListOpen, setLangListOpen] = useState(false);
     const [selectedLanguage, setSelectedLanguage] = useState('언어 선택');
+    const [sortCriteriaOpen, setSortCriteriaOpen] = useState(false);
+    const [selectedSortCriteria, setSelectedSortCriteria] = useState('추천 콘텐츠');
 
     const toggleDropdown = (dropdown: string) => {
         if (dropdown === 'viewMode') setViewModeOpen(!viewModeOpen);
         if (dropdown === 'language') setLangListOpen(!langListOpen);
+        if (dropdown === 'sortCriteria') setSortCriteriaOpen(!sortCriteriaOpen);
     };
 
     const handleSelection = (dropdown: string, value: string) => {
         if (dropdown === 'viewMode') setSelectedViewMode(value);
         if (dropdown === 'language') setSelectedLanguage(value);
+        if (dropdown === 'sortCriteria') setSelectedSortCriteria(value);
         setViewModeOpen(false);
         setLangListOpen(false);
+        setSortCriteriaOpen(false);
     };
 
     return (
@@ -104,18 +109,20 @@ const SubHeader = () => {
                             <div className='float-right text-[14px] text-right'>
                                 <span>정렬 기준</span>
                                 <div className='inline-block relative min-w-[240px] ml-[10px] text-left'>
-                                    <div className='inline-block w-full bg-black border border-[hsla(0,0%,100%,0.9)] h-[1.5rem] tracking-[1px] leading-[1.5rem] pl-[10px] pr-[50px] cursor-pointer hover:bg-[hsla(0,0%,100%,0.1)]'>추천 콘텐츠
+                                    <div onClick={() => toggleDropdown('sortCriteria')} className='inline-block w-full bg-black border border-[hsla(0,0%,100%,0.9)] h-[1.5rem] tracking-[1px] leading-[1.5rem] pl-[10px] pr-[50px] cursor-pointer hover:bg-[hsla(0,0%,100%,0.1)]'>{selectedSortCriteria}
                                         <span className='arrow absolute border-t-[5px] border-l-[5px] border-r-[5px] border-b-0 border-solid border-t-white border-l-transparent border-r-transparent top-[50%] right-[10px] cursor-pointer'></span>
                                     </div>
                                     {/* 정렬 기준 드롭박스 */}
-                                    <div className='absolute left-0 w-full leading-[21px] bg-[rgba(0,0,0,0.9)] border border-[hsla(0,0%,100%,0.15)] text-white whitespace-nowrap z-50 overflow-x-hidden opacity-100 duration-150'>
-                                        <ul className='py-[5px]'>
-                                            <li className='leading-[24px] group'><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>추천 콘텐츠</a></li>
-                                            <li className='leading-[24px] group'><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>출시일순</a></li>
-                                            <li className='leading-[24px] group'><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>오름차순(ㄱ~Z)</a></li>
-                                            <li className='leading-[24px] group'><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>내림차순(Z~ㄱ)</a></li>
-                                        </ul>
-                                    </div>
+                                    {sortCriteriaOpen && (
+                                        <div className='absolute left-0 w-full leading-[21px] bg-[rgba(0,0,0,0.9)] border border-[hsla(0,0%,100%,0.15)] text-white whitespace-nowrap z-50 overflow-x-hidden opacity-100 duration-150'>
+                                            <ul className='py-[5px]'>
+                                                <li className='leading-[24px] group' onClick={() => handleSelection('sortCriteria', '추천 콘텐츠')}><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>추천 콘텐츠</a></li>
+                                                <li className='leading-[24px] group' onClick={() => handleSelection('sortCriteria', '출시일순')}><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>출시일순</a></li>
+                                                <li className='leading-[24px] group' onClick={() => handleSelection('sortCriteria', '오름차순(ㄱ~Z)')}><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>오름차순(ㄱ~Z)</a></li>
+                                                <li className='leading-[24px] group' onClick={() => handleSelection('sortCriteria', '내림차순(Z~ㄱ)')}><a href='#' className='inline-block w-full py-[1px] pl-[10px] pr-[20px] group-hover:underline'>내림차순(Z~ㄱ)</a></li>
+                                            </ul>
+                                        </div>
+                                    )}
                                 </div>
                             </div>
                         </div>
