1d26c3ac99db6f61e630c115ff642d7d7c17faf6
SangHwi <brilyent72@gmail.com>
2025-01-19 19:25:55 +0900

    서브 헤더 디자인 1차 (상단 고정, 배경, 선호하는 설정 부분)

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 const SubHeader = () => {
     return (
-        <div className='sub__header__main'>
+        <div className='sub__header__main fixed top-[70px] w-full h-[68px] z-50 bg-[rgb(20,20,20)]'>
             <div>
-                <div>
-                    <div>
-                        <div>언어별로 찾아보기</div>
-                        <div>
+                <div className='flex h-[68px] px-[4%] items-center'>
+                    <div className='flex flex-grow flex-wrap justify-between'>
+                        <div className='inline-block leading-[18px] mr-[20px] text-[1.5rem]'>언어별로 찾아보기</div>
+                        <div className='flex items-center'>
                             <span>선호하는 설정을 선택하세요</span>
-                            <div>
-                                <div>
-                                    <div>
-                                        <div>원어
-                                            <span className='arrow'></span>
+                            <div className='flex items-center my-[10px]'>
+                                <div className='Dropdown inline-block mr-[10px] align-top'>
+                                    <div className='inline-block text-left relative'>
+                                        <div className='inline-block w-full bg-black border border-[hsla(0,0%,100%,0.9)] h-[1.5rem] tracking-[1px] leading-[1.5rem] pl-[10px] pr-[50px]'>원어
+                                            <span className='arrow inline-block sticky border-t-[5px] border-l-[5px] border-r-[5px] border-b-0 border-solid border-t-white border-l-transparent border-r-transparent h-0 w-0 ml-[8px] right-4 cursor-pointer'></span>
                                         </div>
                                     </div>
                                 </div>
-                                <div>
-                                    <div>
-                                        <div>영어
-                                            <span className='arrow'></span>
+                                <div className='Dropdown inline-block mr-[10px] align-top'>
+                                    <div className='inline-block text-left relative min-w-[240px]'>
+                                        <div className='inline-block w-full bg-black border border-[hsla(0,0%,100%,0.9)] h-[1.5rem] tracking-[1px] leading-[1.5rem] pl-[10px] pr-[50px]'>영어
+                                            <span className='arrow inline-block sticky border-t-[5px] border-l-[5px] border-r-[5px] border-b-0 border-solid border-t-white border-l-transparent border-r-transparent h-0 w-0 ml-[8px] right-4 cursor-pointer'></span>
                                         </div>
                                     </div>
                                 </div>
@@ -28,7 +28,7 @@ const SubHeader = () => {
                             <div>
                                 <span>정렬 기준</span>
                                 <div>
-                                    <div>추천 콘텐츠
+                                    <div>
                                         <span className='arrow'></span>
                                     </div>
                                 </div>
