bc47faa55921c42f6c7e4ab49e2bf648d782aef3
seongmin <min@seongminui-MacBookPro.local>
2025-01-20 11:24:01 +0900

    더미카드 클릭 시 디테일 이동하도록 적용

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { MediaType } from "../models/Media";
 
 export interface CardProps {
@@ -8,24 +7,14 @@ export interface CardProps {
 	type: MediaType;
 }
 
-
 const Card: React.FC<CardProps> = ({ backdrop_path }) => {
-	const navigate = useNavigate();
-
-	const handleClickCard = () => {
-		// navigate("/movie/1241982");
-		navigate("/tv/1408");
-	};
-
 	return (
 		<div className="relative">
-			<button onClick={handleClickCard}>
-				<img
-					src={backdrop_path}
-					alt=""
-					className="h-auto object-cover rounded-sm"
-				/>
-			</button>
+			<img
+				src={backdrop_path}
+				alt=""
+				className="h-auto object-cover rounded-sm"
+			/>
 		</div>
 	);
 };
