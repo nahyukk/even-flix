82f8c43bc9d8c7456db426362aca1d4668825229
LJIEUN <eun773c@daum.net>
2025-01-20 11:53:03 +0900

    error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,44 +9,49 @@ import DetailModal from "./components/DetailModal/page";
 import { MediaType } from "./models/Media";
 import MyList from "./pages/MyList/page";
 import { FavoriteProvider } from "./context/FavoriteContext";
+import MoviePage from "./pages/MoviePage/page";
 
 const Layout = () => {
-  return (
-    <div>
-      <NavBar />
+	return (
+		<div>
+			<NavBar />
 
-      <Outlet />
+			<Outlet />
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 };
 
 function App() {
-  const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location };
-  const background = state?.backgroundLocation;
-  return (
-    <FavoriteProvider>
-      <div className="app">
-        <Routes location={background || location}>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="browse" element={<HomePage />} />
-            <Route path="browse/my-list" element={<MyList />} />
-            <Route
-              path="movie/:id"
-              element={<DetailModal mediaType={MediaType.MOVIE} />}
-            />
-            <Route
-              path="tv/:id"
-              element={<DetailModal mediaType={MediaType.TV} />}
-            />
-          </Route>
-        </Routes>
-      </div>
-    </FavoriteProvider>
-  );
+	const location = useLocation();
+	const state = location.state as { backgroundLocation?: Location };
+	const background = state?.backgroundLocation;
+	return (
+		<FavoriteProvider>
+			<div className="app">
+				<Routes location={background || location}>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<HomePage />} />
+						<Route path="browse" element={<HomePage />} />
+						<Route path="browse/my-list" element={<MyList />} />
+						<Route
+							path="browse/genre/34399"
+							element={<MoviePage type={MediaType.MOVIE} />}
+						/>
+						<Route
+							path="movie/:id"
+							element={<DetailModal mediaType={MediaType.MOVIE} />}
+						/>
+						<Route
+							path="tv/:id"
+							element={<DetailModal mediaType={MediaType.TV} />}
+						/>
+					</Route>
+				</Routes>
+			</div>
+		</FavoriteProvider>
+	);
 }
 
 export default App;
